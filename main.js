451044ffe6c2c219b0921c2ac93bb0e766de96f4
QuocDinh <quochit879@gmail.com>
2022-05-31 17:13:48 +0700

    addFile

---
+++ main.js
@@ -0,0 +1,11 @@
+
+function InTerGer(value){
+  return (Number.isInteger(value) && value > 0);
+}
+
+const buttonsw = document.getElementById('button1');
+const inputsw = document.querySelector('#sub');
+const printf = document.querySelector('#print');
+
+
+document.getElementById('button1').addEventListener('click', alert('1234'));
